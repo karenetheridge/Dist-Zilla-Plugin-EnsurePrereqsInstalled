d1648f49112e65db4b10763edc7ce4c4dd3fb180
Karen Etheridge <ether@cpan.org>
2015-09-05 12:13:47 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/EnsurePrereqsInstalled.pm
+++ lib/Dist/Zilla/Plugin/EnsurePrereqsInstalled.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::EnsurePrereqsInstalled;
 # ABSTRACT: Ensure at build time that all prereqs, including developer, are satisfied
 # KEYWORDS: plugin toolchain prerequisites dependencies modules metadata
-# vim: set ts=8 sts=4 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.009';
 
